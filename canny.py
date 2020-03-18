fd0569bca3eb20b91cc604223561601b85f039aa
Soutrik <soutrikmaiti.uta@gmail.com>
2020-03-17 21:15:59 -0700

    Files added

---
+++ canny.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('test_image.jpg')
+lane_image = np.copy(image)
+gray = cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY)
+blur = cv2.GaussianBlur(gray,(5,5), 0)
+canny = cv2.Canny(blur,50,150)
+cv2.imshow("result",canny)
+cv2.waitKey(0)
